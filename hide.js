e9812d9ff4fc772eb963c3bb86b1411d10bb10bd
Yvanpits <pepitoyvan050@gmail.com>
2024-06-10 15:40:05 +0800

    Add files via upload

---
+++ hide.js
@@ -0,0 +1,57 @@
+function Menu() {
+            var content = document.getElementById("content");
+            if (content.style.display === "block") {
+                content.style.height = "0";
+                setTimeout(function() {
+                    content.style.display = "none";
+                }, 0.5);
+            } else {
+                content.style.display = "block";
+                setTimeout(function() {
+                    content.style.height = "auto";
+                }, 0.5);
+            }
+        }
+        
+ function Gallery() {
+            var content = document.getElementById("content1");
+            if (content.style.display === "block") {
+                content.style.height = "0";
+                setTimeout(function() {
+                    content.style.display = "none";
+                }, 0.5);
+            } else {
+                content.style.display = "block";
+                setTimeout(function() {
+                    content.style.height = "auto";
+                }, 0.5);
+            }
+        }
+function About() {
+            var content = document.getElementById("content2");
+            if (content.style.display === "block") {
+                content.style.height = "0";
+                setTimeout(function() {
+                    content.style.display = "none";
+                }, 0.5);
+            } else {
+                content.style.display = "block";
+                setTimeout(function() {
+                    content.style.height = "auto";
+                }, 0.5);
+            }
+        }
+function Services() {
+            var content = document.getElementById("content3");
+            if (content.style.display === "block") {
+                content.style.height = "0";
+                setTimeout(function() {
+                    content.style.display = "none";
+                }, 0.5);
+            } else {
+                content.style.display = "block";
+                setTimeout(function() {
+                    content.style.height = "auto";
+                }, 0.5);
+            }
+        }
